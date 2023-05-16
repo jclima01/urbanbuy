7465bf0bf3711466147ff790025d565ffa7b2b22
Juan Cruz Lima Caramico <juan.mdq22@gmail.com>
2023-05-15 22:58:24 -0300

    model Admin edited, borré propiedad id para que la genere automaticamente mongo, model ClientAdmin added

--- api/src/models/Users/Admin.js
+++ api/src/models/Users/Admin.js
@@ -3,12 +3,11 @@ const Schema = mongoose.Schema;
 
 const AdminSchema = new Schema(
   {
-    id: { type: String },
-    fullName: { type: String, required: true, unique: true },
-    clients: { type: Array, default: []  },
-    email: { type: String, required: true, unique: true},
-    password:  { type: String },
-    permissions:  { type: String },
+    fullName: { type: String },
+    clients: { type: Array, default: [] },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    permissions: { type: String },
   },
   { timestamps: true }
 );
---
+++ api/src/models/Users/ClientAdmin.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ClientAdminSchema = new Schema(
+  {
+    fullName: { type: String},
+    users: { type: Array, default: [] },
+    logo: { type: String},
+    optionsDesing: { type: Array, default: [] },
+    catalogue: { type: Array, default: [] },
+
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    permissions: { type: String },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("ClientAdmin", ClientAdminSchema);
