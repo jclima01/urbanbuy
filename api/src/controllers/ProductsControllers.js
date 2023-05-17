815797ecea66a11c6f53aa569f04dc0725541630
Jose <malgor.jose@gmail.com>
2023-05-17 17:16:43 -0300

    ProductHandlers-ProductControllers(update and deletet)

--- api/src/controllers/ProductsControllers.js
+++ api/src/controllers/ProductsControllers.js
@@ -7,19 +7,19 @@ const mongoose = require('mongoose');
 //All products
 const getAllProducts = async () => {
     const dataBaseProducts = await Product.find({});
-    return dataBaseProducts.map(product => product.toJSON());
+    return dataBaseProducts;
 };
 
 //By Name
 const getProductName = async (name) => {
     const dataBaseProducts = await Product.find({ productName: name });
-    return dataBaseProducts.map(product => product.toJSON());
+    return dataBaseProducts;
 };
 
 //By Id
 const getProductById = async (id) => {
     const dataBaseProducts = await Product.findById(id);
-    return dataBaseProducts.toJSON();
+    return dataBaseProducts;
 };
 
 
@@ -35,7 +35,20 @@ const createNewProduct = async (productName, description, categories, stocks, im
 
 //PUT
 
-const updateProduct = {
+const updateProduct = async (productId, updateData) => {
+    const product = await Product.findById(productId);
+    if (product) {
+        product.productName = updatedData.productName;
+        product.description = updatedData.description;
+        product.categories = updatedData.categories;
+        product.stocks = updatedData.stocks;
+        product.imageUrl = updatedData.imageUrl;
+        product.price = updatedData.price;
+        product.rating = updatedData.rating;
+        product.clientAdmin = updatedData.clientAdmin;
+    }
+    const updatedProduct = await product.save();
+    return updatedProduct;
 
 };
 
