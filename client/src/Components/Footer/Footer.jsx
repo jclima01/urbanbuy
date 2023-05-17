de3cb037db9804e81744f81a29b25f315035a414
Alejandro Villamayor <avillamayor720@gmail.com>
2023-05-17 19:00:00 -0300

    foother completo y botones del nav

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -4,13 +4,19 @@ import "../Footer/Footer.css";
 function Footer() {
   return (
     <div className="container-footer">
-      <p className="contacto">Datos de contacto :</p>
-      <ul className="lista">
-        <li>email</li>
-        <li>Telefono</li>
-        <li>Horarios de atencion</li>
+      <p className="contacto">Datos de contacto:</p>
+      <ul className="contacto">
+        <li>urbanbuy8@gmail.com</li>
+        <li>+1 01-2345-6789</li>
+        <li>Horarios de atencion
+          8:00 a 20:00 (GMT-3)
+        </li>
       </ul>
-      <p className="copy">copyright@2023 UrbanBuy </p>
+      <p className="copy">
+    © 2023 UrbanBuy. Todos los derechos reservados.
+    <a href="https://github.com/tu-usuario" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+    <a href="https://linkedin.com/in/tu-usuario" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a>
+  </p>
     </div>
   );
 }
