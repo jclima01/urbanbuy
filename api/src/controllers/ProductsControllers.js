b25dcfad34acb59478f9796c645e4d5c85788aec
Jose <malgor.jose@gmail.com>
2023-05-16 21:02:24 -0300

    ProductHandlers-ProductControllers(get y post)

--- api/src/controllers/ProductsControllers.js
+++ api/src/controllers/ProductsControllers.js
@@ -1,23 +1,35 @@
+const Product = require("../models/Product");
+const mongoose = require('mongoose');
+
+
 //GETS
 
 //All products
-const getAllProducts = {
-
-    
+const getAllProducts = async () => {
+    const dataBaseProducts = await Product.find({});
+    return dataBaseProducts.map(product => product.toJSON());
 };
 
-const getProductName = {
+//By Name
+const getProductName = async (name) => {
+    const dataBaseProducts = await Product.find({ productName: name });
+    return dataBaseProducts.map(product => product.toJSON());
+};
 
+//By Id
+const getProductById = async (id) => {
+    const dataBaseProducts = await Product.findById(id);
+    return dataBaseProducts.toJSON();
 };
 
 
-const getProductForId = {};
 
 
 //POST
 
-const creteNewProduct = {
-
+const createNewProduct = async (productName, description, categories, stocks, imageUrl, price, rating, clientAdmin) => {
+    const newProduct = await new Product({ productName, description, categories, stocks, imageUrl, price, rating, clientAdmin })
+    return newProduct;
 };
 
 
@@ -37,8 +49,8 @@ const deleteProduct = {
 module.exports = {
     getAllProducts,
     getProductName,
-    getProductForId,
-    creteNewProduct,
+    getProductById,
+    createNewProduct,
     updateProduct,
     deleteProduct
 }
--- api/src/handlers/ProductHandlers.js
+++ api/src/handlers/ProductHandlers.js
@@ -1,24 +1,41 @@
 // import controllers
+const { getAllProducts, getProductName, getProductById, createNewProduct, updateProduct, deleteProduct } = require("../controllers/ProductsControllers")
 
 
 const getProductsHandler = async (req, res) => {
-    try {
-
-    } catch (error) {
-      res.status(400).json({ error: error.message });
+  const { name } = req.query;
+  const { id } = req.params;
+  try {
+    let result;
+    if (id) {
+      result = await getProductById(id);
+    } else if (name) {
+      result = await getProductName(name);
+    } else {
+      result = await getAllProducts();
     }
-  };
+    res.status(200).json(result);
 
-  const postProductHandler = async (req, res) => {
-    try {
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  };
-  
-  module.exports = {
-    getProductsHandler,
-    postProductHandler
-  };
-  
+
+
+
+const postProductHandler = async (req, res) => {
+  const {productName, description, categories, stocks, imageUrl, price, rating, clientAdmin} = req.body;
+  try {
+    const newProduct = await createNewProduct(productName, description, categories, stocks, imageUrl, price, rating, clientAdmin);
+    res.status(200).json({message: "producto creado", newProduct: newProduct});
+
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  getProductsHandler,
+  postProductHandler
+};
