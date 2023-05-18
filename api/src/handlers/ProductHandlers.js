a74256717a8f45b9024209456b0fdeb4d670086b
Jose <malgor.jose@gmail.com>
2023-05-18 16:12:47 -0300

    ProductHandlers-ProductControllers(arregle el get id y get name)

--- api/src/handlers/ProductHandlers.js
+++ api/src/handlers/ProductHandlers.js
@@ -6,12 +6,10 @@ const { getAllProducts } = require("../controllers/ProductsControllers");
 
 const getProductsHandler = async (req, res) => {
   const { name } = req.query;
-  const { id } = req.params;
+ 
   try {
     let result;
-    if (id) {
-      result = await getProductById(id);
-    } if (!id && name) {
+       if (name) {
       result = await getProductName(name);
     } else {
       result = await getAllProducts();
@@ -23,6 +21,20 @@ const getProductsHandler = async (req, res) => {
     }
   };
 
+
+const getProductsHandlerById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+  let result;
+if (id) {
+  result = await getProductById(id);
+}
+  res.status(200).json(result);
+} catch (error) {
+  res.status(400).json({ error: error.message });
+}
+};
 
 
 
@@ -68,6 +80,7 @@ module.exports = {
   getProductsHandler,
   postProductHandler,
   updateProductHandler,
-  deletedProductHandler
+  deletedProductHandler,
+  getProductsHandlerById
 
 };
