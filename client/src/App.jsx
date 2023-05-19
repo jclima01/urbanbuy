a229c7bd428f487473859fd463e08185f0d5dd80
ZeroSwordDev <Miguelworking1@gmail.com>
2023-05-19 16:48:55 -0400

    Se realiza cambios y quitamos el componente SearchBar de App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import {  Routes, Route } from "react-router-dom";
-import {  useSelector } from 'react-redux';
+import { Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 import DashBoard from "./pages/DashBoard/DashBoard";
 import SideBarDashBoard from "./Components/SideBarDashBoard/SideBarDashBoard";
 import NavBarDashBoard from "./Components/NavBarDashBoard/NavBarDashBoard";
@@ -11,41 +11,20 @@ import DashBoardShipping from "./pages/DashBoardShipping/DashBoardShipping";
 import DashBoardSettings from "./pages/DashBoardSettings/DashBoardSettings";
 import Home from "./Components/Home/Home";
 import FormLogin from "./Components/FormLogin/FormLogin";
-import SignIn from "./Components/SignIn/SignIn"
-
-import SearchBar from './SearchBar/SearchBar.jsx'
-import { useState } from "react";
-import { Products } from "./data"  
-
+import SignIn from "./Components/SignIn/SignIn";
 
 function App() {
+  const clientAdmin = useSelector((state) => state.UserSession);
 
-
-  const clientAdmin = useSelector(state => state.UserSession)
-  console.log('clientAdmin', clientAdmin)
-  const [searches, setSearches] = useState([])
-
-  function handleSearch(searchTerm) {
-    const result = Products.some(p => p.productName.toLowerCase().includes(searchTerm.toLowerCase()))
-      if (result){
-        const result = Products.filter(p => p.productName.toLowerCase().includes(searchTerm.toLowerCase()))
-        setSearches(result)
-        console.log('result: ', result)
-        console.log('searchTerm: ', searchTerm)
-      } else {
-        setSearches([])
-      }
-  }
   return (
     <>
-
-      { clientAdmin  ? (
+      {clientAdmin ? (
         <div className="d-flex vh-100 vw-100 ">
           <SideBarDashBoard />
           <div className="d-flex flex-column">
             <NavBarDashBoard />
             <Routes>
-              <Route  index path="/dashBoard/home" element={<DashBoard />} />
+              <Route index path="/dashBoard/home" element={<DashBoard />} />
               <Route path="/dashBoard/User" element={<DashBoardUser />} />
               <Route path="/dashBoard/Edit" element={<DashBoardEdit />} />
               <Route
@@ -65,16 +44,13 @@ function App() {
         </div>
       ) : (
         <Routes>
-          
-          <Route path="/" element={<Home/>} />   {/* LadingPage */}
+          <Route path="/" element={<Home />} /> {/* LadingPage */}
           <Route path="/login" element={<FormLogin />} />
           <Route path="/SignIn" element={<SignIn />} />
         </Routes>
       )}
-
-</>  
-);
+    </>
+  );
 }
-
 
 export default App;
