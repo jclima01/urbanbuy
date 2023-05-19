83100267e71050db46bcb2818b7344a3a030d0d5
unknown <juan.mdq22@gmail.com]>
2023-05-19 05:02:43 -0300

    adding products actions

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -5,8 +5,125 @@ export const LOGIN_CLIENT_ADMIN = "LOGIN_CLIENT_ADMIN";
 export const REGISTER_CLIENT_ADMIN = "REGISTER_CLIENT_ADMIN";
 export const LOGIN_USER = "LOGIN_USER";
 export const REGISTER_USER = "REGISTER_USER";
+export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
+export const GET_PRODUCT_BY_ID = "GET_PRODUCT_BY_ID";
+export const POST_NEW_PRODUCT = "POST_NEW_PRODUCT";
+export const EDIT_PRODUCT = "EDIT_PRODUCT";
+export const DELETE_PRODUCT = "DELETE_PRODUCT";
+export const deleteProduct = (productId) => {
+  try {
+    return async function (dispatch) {
+      const { data } = await axios.post(
+        `http://localhost:2800/products/:${productId}`
+      );
+      return dispatch({
+        type: DELETE_PRODUCT,
+        payload: data,
+      });
+    };
+    // eslint-disable-next-line no-unreachable
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
 
-export const loginAdmin = (email,password) => {
+export const editProduct = (
+  productId,
+  productName,
+  description,
+  categoriesIds,
+  stocks,
+  imageUrl,
+  price,
+  rating
+) => {
+  try {
+    return async function (dispatch) {
+      const { data } = await axios.post(
+        `http://localhost:2800/products/:${productId}`,
+        {
+          productName,
+          description,
+          categoriesIds,
+          stocks,
+          imageUrl,
+          price,
+          rating,
+        }
+      );
+      return dispatch({
+        type: GET_PRODUCT_BY_ID,
+        payload: data,
+      });
+    };
+    // eslint-disable-next-line no-unreachable
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+export const postNewProduct = (
+  productName,
+  description,
+  categories,
+  stocks,
+  imageUrl,
+  price,
+  rating,
+  clientAdmin
+) => {
+  try {
+    return async function (dispatch) {
+      const { data } = await axios.post("http://localhost:2800/products", {
+        productName,
+        description,
+        categories,
+        stocks,
+        imageUrl,
+        price,
+        rating,
+        clientAdmin,
+      });
+      return dispatch({
+        type: POST_NEW_PRODUCT,
+        payload: data,
+      });
+    };
+    // eslint-disable-next-line no-unreachable
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+export const getProductById = (productId) => {
+  try {
+    return async function (dispatch) {
+      const { data } = await axios.post(
+        `http://localhost:2800/products/:${productId}`
+      );
+      return dispatch({
+        type: GET_PRODUCT_BY_ID,
+        payload: data,
+      });
+    };
+    // eslint-disable-next-line no-unreachable
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+export const getAllProducts = () => {
+  try {
+    return async function (dispatch) {
+      const { data } = await axios.post("http://localhost:2800/products");
+      return dispatch({
+        type: GET_ALL_PRODUCTS,
+        payload: data,
+      });
+    };
+    // eslint-disable-next-line no-unreachable
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+export const loginAdmin = (email, password) => {
   try {
     return async function (dispatch) {
       const { data } = await axios.post("http://localhost:2800/admin/login", {
@@ -18,27 +135,32 @@ export const loginAdmin = (email,password) => {
         payload: data,
       });
     };
+    // eslint-disable-next-line no-unreachable
   } catch (err) {
     throw new Error(err.message);
   }
 };
-export const loginClientAdmin = (email,password) => {
+export const loginClientAdmin = (email, password) => {
   try {
     return async function (dispatch) {
-      const { data } = await axios.post("http://localhost:2800/clientAdmin/login", {
-        email,
-        password,
-      });
+      const { data } = await axios.post(
+        "http://localhost:2800/clientAdmin/login",
+        {
+          email,
+          password,
+        }
+      );
       return dispatch({
         type: LOGIN_CLIENT_ADMIN,
         payload: data,
       });
     };
+    // eslint-disable-next-line no-unreachable
   } catch (err) {
     throw new Error(err.message);
   }
 };
-export const loginUser = (email,password) => {
+export const loginUser = (email, password) => {
   try {
     return async function (dispatch) {
       const { data } = await axios.post("http://localhost:2800/user/login", {
@@ -50,13 +172,13 @@ export const loginUser = (email,password) => {
         payload: data,
       });
     };
+    // eslint-disable-next-line no-unreachable
   } catch (err) {
     throw new Error(err.message);
   }
 };
 
-
-export const registerAdmin = (email,password) => {
+export const registerAdmin = (email, password) => {
   try {
     return async function (dispatch) {
       await axios.post("http://localhost:2800/admin/register", {
@@ -68,11 +190,12 @@ export const registerAdmin = (email,password) => {
         type: REGISTER_ADMIN,
       });
     };
+    // eslint-disable-next-line no-unreachable
   } catch (err) {
     throw new Error(err.message);
   }
 };
-export const registerClientAdmin = (email,password) => {
+export const registerClientAdmin = (email, password) => {
   try {
     return async function (dispatch) {
       await axios.post("http://localhost:2800/clientAdmin/register", {
@@ -84,11 +207,12 @@ export const registerClientAdmin = (email,password) => {
         type: REGISTER_CLIENT_ADMIN,
       });
     };
+    // eslint-disable-next-line no-unreachable
   } catch (err) {
     throw new Error(err.message);
   }
 };
-export const registerUser = () => {
+export const registerUser = (email, password) => {
   try {
     return async function (dispatch) {
       await axios.post("http://localhost:6800/admin/register", {
@@ -100,6 +224,7 @@ export const registerUser = () => {
         type: REGISTER_USER,
       });
     };
+    // eslint-disable-next-line no-unreachable
   } catch (err) {
     throw new Error(err.message);
   }
--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,13 +1,25 @@
-import { LOGIN_ADMIN, LOGIN_CLIENT_ADMIN, LOGIN_USER, REGISTER_ADMIN, REGISTER_CLIENT_ADMIN, REGISTER_USER } from "../actions/index.js";
+import { GET_ALL_PRODUCTS, GET_PRODUCT_BY_ID, LOGIN_ADMIN, LOGIN_CLIENT_ADMIN, LOGIN_USER, REGISTER_ADMIN, REGISTER_CLIENT_ADMIN, REGISTER_USER } from "../actions/index.js";
 
 const initialState = {
   admin:{},
   clientAdmin:{},
   user:{},
+  products:[],
+  product:{}
 };
 
 const rootReducer = (state = initialState, { type, payload }) => {
   switch (type) {
+    case GET_PRODUCT_BY_ID:
+      return {
+        ...state,
+        product: {...payload},
+      };
+    case GET_ALL_PRODUCTS:
+      return {
+        ...state,
+        products: [...payload],
+      };
     case LOGIN_ADMIN:
       return {
         ...state,
