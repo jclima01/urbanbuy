6183b5bc71b0d97fecf9a25b2bfb5a5e27ce18d2
emamanriquez <emamanriquez1991@gmail.com>
2023-05-18 22:04:43 -0300

    solucionando conflictos

---
+++ api/src/routes/index.js
@@ -0,0 +1,24 @@
+const { Router } = require('express');
+// Importar todos los routers;
+const ProductRouter = require('./ProductsRoutes.js');
+const UsersRouter = require('./UserRoutes.js');
+const AdminRouter = require('./AdminRoutes.js');
+const ClientAdminRouter = require('./ClientAdminRoutes.js');
+const CategoryRoutes = require('./CategoryRoutes.js')
+
+const mainRouter = Router();
+
+// Configurar los routers
+
+mainRouter.use('/products', ProductRouter);
+mainRouter.use('/users', UsersRouter);
+mainRouter.use('/admin', AdminRouter);
+mainRouter.use('/clientAdmin', ClientAdminRouter);
+mainRouter.use('/user', UsersRouter);
+mainRouter.use('/category', CategoryRoutes);
+
+// mainRouter.use('/users', UsersRouter);
+
+
+
+module.exports = mainRouter;
