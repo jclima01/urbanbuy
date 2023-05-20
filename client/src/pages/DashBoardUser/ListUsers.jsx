e7974f999b6e94c769b60160f2db65c43e7d5bc7
emamanriquez <emamanriquez1991@gmail.com>
2023-05-20 18:54:23 -0300

    push test dashboard user

--- client/src/pages/DashBoardUser/ListUsers.jsx
+++ client/src/pages/DashBoardUser/ListUsers.jsx
@@ -7,6 +7,7 @@ const DashBoardListUsers = () => {
                   boton para entrar al detalle */ 
                   }
                   <h1>Test User</h1>
+                  <h2>Lista de usuarios avatar</h2>
                   <span><img src="avatar" alt="nombre avatar"/></span>
                 </div>
     )
