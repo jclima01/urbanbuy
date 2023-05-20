39c92bd348f05d0d4f5a58c90b02f247440ab40e
ALBERTO EZEQUIEL IRACE <103865149+Eze323@users.noreply.github.com>
2023-05-20 18:54:46 -0300

    dividimos el trabajo con ema y eze en 2 componentes

---
+++ client/src/pages/DashBoardUser/ListUsers.jsx
@@ -0,0 +1,15 @@
+const DashBoardListUsers = () => {
+    return (
+        <div className="contentAllUser">
+                  {/* trabajas ema con los titulos de columnas
+                  -------------------------------
+                  lista de usuarios avatar ,
+                  boton para entrar al detalle */ 
+                  }
+                  <h1>Test User</h1>
+                  <span><img src="avatar" alt="nombre avatar"/></span>
+                </div>
+    )
+  }
+  
+  export default DashBoardListUsers
