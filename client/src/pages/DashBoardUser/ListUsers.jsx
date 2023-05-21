c19fc01041ef362e6a99d1f1610bb6624cb96bd5
emamanriquez <emamanriquez1991@gmail.com>
2023-05-21 00:55:41 -0300

    se adelanto en las ul/li, faltaria estilos y funcionalidad de button  view user

--- client/src/pages/DashBoardUser/ListUsers.jsx
+++ client/src/pages/DashBoardUser/ListUsers.jsx
@@ -1,16 +1,59 @@
+import '../DashBoardUser/ListUsers.css'
+import { User } from '../../data.js';
+
+
 const DashBoardListUsers = () => {
-    return (
-        <div className="contentAllUser">
-                  {/* trabajas ema con los titulos de columnas
+
+//   const listaUser = {};
+//   datos.forEach(User => {
+//     if (!listaUser[User.id]) {
+//       const li = document.createElement('li');
+//       li.textContent = `${User.avatar} - ${User.email}`;
+//       ul.appendChild(li);
+// }})
+  return (
+  
+    <div className="contentAllUser">
+      {/* trabajas ema con los titulos de columnas
                   -------------------------------
                   lista de usuarios avatar ,
-                  boton para entrar al detalle */ 
-                  }
-                  <h1>Test User</h1>
-                  <h2>Lista de usuarios avatar</h2>
-                  <span><img src="avatar" alt="nombre avatar"/></span>
-                </div>
-    )
-  }
-  
-  export default DashBoardListUsers
+                  boton para entrar al detalle */}
+            <div>            
+                <ul className='listUl'>
+                  <li>Avatar </li> 
+                 
+                  <li>FullName </li>
+                 
+                  <li>Email </li>
+                  
+                  <li>Permision </li>
+                  
+                  <li>Options</li>
+                </ul>
+            </div> 
+              <div >
+               <ul className='datosUser'>
+                 {User.map(user => <li>
+                  <img src={user.avatar} alt={user.fullName} width="50px" />
+                  <h6>{user.fullName}</h6>
+                  <h6>{user.email}</h6>
+                  <h6>{user.permissions}</h6>
+                  <button>View User</button>
+                 </li>)}
+                  
+                  </ul>
+
+             </div>
+            <span>
+              
+            </span>
+    </div>
+  );
+};
+
+
+
+
+
+
+export default DashBoardListUsers;
