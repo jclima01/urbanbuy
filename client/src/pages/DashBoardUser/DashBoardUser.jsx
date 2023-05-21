85614231ac630862c8626a892ead581bd615666a
ALBERTO EZEQUIEL IRACE <103865149+Eze323@users.noreply.github.com>
2023-05-21 06:06:43 -0300

    haciendo dashboard user parte superior

--- client/src/pages/DashBoardUser/DashBoardUser.jsx
+++ client/src/pages/DashBoardUser/DashBoardUser.jsx
@@ -4,12 +4,54 @@ import DashBoardListUsers from "../DashBoardUser/ListUsers";
 import DashBoardNavUsers from "./NavUsers";
 
 const DashBoardUser = () => {
+  const navTab={
+    width:"100%",
+    display:"flex",
+    flexDirection:"row",
+    flexWrap: "nowrap",
+    justifyContent:"flex-start",
+    alignItems:"center",
+    alignContent: "center",
+    overflow:"hidden",
+    paddingTop:"7px",
+    height:"50px"
+
+};
+
+ const tab={    
+    backgroundColor: "rgb(255, 127, 42)",
+    width:"max-content",
+    padding: "11px 20px 11px 20px",
+    borderRadius:"10px",
+    color:"white",
+    marginRight:"8px",
+    cursor:"pointer",
+
+
+}
+const tabActive={
+    backgroundColor: "white",
+    width:"max-content",
+    padding: "11px 20px 11px 20px",
+    borderRadius:"10px",
+    color:"black",
+    border:"2px solid rgb(255, 127, 42)",
+    marginRight:"8px",
+    
+}
   return (
+    <div className="contieneTodoDashboardUsers">
+    <div style={navTab}>
+                    <div style={tabActive}>All Users</div>
+                    <div style={tab}>User Detail</div>
+    </div>
     <div className="contentDashboardUsers">
                   <DashBoardNavUsers/> {/* Eze */}
+                  
                    <DashBoardListUsers/>{/* Ema */}
                 
 
+    </div>
     </div>
   )
 }
--- client/src/pages/DashBoardUser/NavUsers.jsx
+++ client/src/pages/DashBoardUser/NavUsers.jsx
@@ -1,8 +1,47 @@
+import { CiSearch } from "react-icons/ci"
+
 const DashBoardNavUsers = () => {
-    return (
-      
-      <div className="navtab">
-                    {/* 
+ 
+    const contentOrderSearchAndFilters={
+        with:"95%",
+        height:"100px",
+        backgroundColor:"gray",
+        borderRadius:"15px 15px 0px 0px",
+        display:"flex",
+        flexDirection:"row",
+        flexWrap:"nowrap",
+        alignContent: "center",
+        justifyContent:"space-evenly",
+        alignItems: "center"
+    }
+    const ordenamiento={
+        backgroundColor:"#EFE7E7",
+        width:"150px",
+        borderRadius:"10px",
+        padding:"15px",
+        border:"none",
+        textAlign:"center",
+        color:"rgb(118, 118, 118)"
+    }
+    const filters={
+        backgroundColor:"rgb(255, 127, 42)",
+        width:"150px",
+        borderRadius:"10px",
+        padding:"15px",
+        border:"none",
+        color:"white",
+        textAlign:"center"
+    }
+    const inputSearchUser={
+        display:"flex",
+        backgroundColor:"#EFE7E7",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: "15px",
+        padding: "7px",
+        width: "800px"
+    }
+    {/* 
                     pestañas all users - user detail
                     ------------------------------
                     componente ordenamiento
@@ -13,9 +52,34 @@ const DashBoardNavUsers = () => {
                     ----------------------
                     componente filters
                     */ }
-                    <div className="tab">All User</div>
-                    <div className="tab">User Detail</div>
+    return (
+        <>
+                 
+                  <div style={contentOrderSearchAndFilters}>
+                        <div className="contentOrdenamiento">
+                        <select style={ordenamiento}>
+                            <option value="A-Z">A-Z</option>
+                            <option value="Z-A">Z-A</option>
+                        </select>
+                        </div>
+                        <div className="contentSearchUsers">
+                                <div className=" input-container-navbar" style={inputSearchUser}>
+                                    <input
+                                        type="text"
+                                        placeholder="Search Users..."
+                                        className="inputsearch-navbar"
+                                    />
+                                    <hr />
+                                    <CiSearch size={25} cursor={"pointer"} />
+                                </div>
+                        </div>
+                        <div className="filters">
+                            <select style={filters}>
+                                <option value="Filters">Filters</option>
+                            </select>
+                        </div>
                   </div>
+        </>
     )
   }
   
