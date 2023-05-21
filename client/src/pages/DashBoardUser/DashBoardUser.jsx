e4b939c34bd1a435bb3859d3ad377c8d4a33c8c2
ALBERTO EZEQUIEL IRACE <103865149+Eze323@users.noreply.github.com>
2023-05-21 19:22:38 -0300

    update componente filter search filter

--- client/src/pages/DashBoardUser/DashBoardUser.jsx
+++ client/src/pages/DashBoardUser/DashBoardUser.jsx
@@ -1,17 +1,58 @@
-
 import "../DashBoardUser/DashBoardUser.css";
 import DashBoardListUsers from "../DashBoardUser/ListUsers";
 import DashBoardNavUsers from "./NavUsers";
+import PaginadoUser from "./PaginadoUser";
 
 const DashBoardUser = () => {
-  return (
-    <div className="contentDashboardUsers">
-                  <DashBoardNavUsers/> {/* Eze */}
-                   <DashBoardListUsers/>{/* Ema */}
-                
+  const navTab = {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    alignContent: "center",
+    overflow: "hidden",
+    paddingTop: "7px",
+    height: "50px",
+  };
 
+  const tab = {
+    backgroundColor: "#ff7f2a",
+    width: "max-content",
+    padding: "11px 20px 11px 20px",
+    borderRadius: "10px",
+    color: "white",
+    marginRight: "8px",
+    cursor: "pointer",
+  };
+  const tabActive = {
+    backgroundColor: "white",
+    width: "max-content",
+    padding: "11px 20px 11px 20px",
+    borderRadius: "10px",
+    color: "black",
+    border: "2px solid #ff7f2a",
+    marginRight: "8px",
+  };
+  return (
+    <div className="contieneTodoDashboardUsers">
+      <div className="navegateUser">
+        <div style={navTab}>
+          <div style={tabActive}>All Users</div>
+          <div style={tab}>User Detail</div>
+        </div>
+        <div className="paginationUsers">
+          <PaginadoUser />
+        </div>
+      </div>
+      <div className="contentDashboardUsers">
+        <DashBoardNavUsers /> {/* Eze */}
+        <DashBoardListUsers />
+        {/* Ema */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default DashBoardUser;
