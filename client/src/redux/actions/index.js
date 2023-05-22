4926d8c8fdb2b7d70fe84444d202a147426c8d28
ZeroSwordDev <Miguelworking1@gmail.com>
2023-05-22 00:45:02 -0400

    Realizamos un la implementacion del modulo de producstos

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -150,7 +150,8 @@ export const getProductById = (productId) => {
 export const getAllProducts = (clientAdminId) => {
   try {
     return async function (dispatch) {
-      const { data } = await axios.get(`http://localhost:2800/products/:${clientAdminId}`);
+      const  {data}  = await axios.get(`http://localhost:2800/products/${clientAdminId}`);
+      console.log('data', data)
       return dispatch({
         type: GET_ALL_PRODUCTS,
         payload: data,
