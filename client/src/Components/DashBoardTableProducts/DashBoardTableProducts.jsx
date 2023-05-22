5d8657ddaf60a9f795bceca2a401b581243b40d4
Juan Cruz Lima Caramico <juan.mdq22@gmail.com>
2023-05-22 19:11:52 -0300

    fix navigation

--- client/src/Components/DashBoardTableProducts/DashBoardTableProducts.jsx
+++ client/src/Components/DashBoardTableProducts/DashBoardTableProducts.jsx
@@ -1,35 +1,32 @@
+import Table from "react-bootstrap/esm/Table";
+import DashBoardTableCardProducts from "./DashBoardTableCardProducts/DashBoardTableCardProducts";
 
-import Table from 'react-bootstrap/esm/Table'
-import DashBoardTableCardProducts from './DashBoardTableCardProducts/DashBoardTableCardProducts'
-
-const DashBoardTableProducts = ({Products}) => {
+const DashBoardTableProducts = ({ products }) => {
   return (
-    <Table striped="columns"
-    responsive
-    borderless='false'
-    hover='true'
-    variant='lightgray'>
-            <thead>
-              <tr>
-                <th style={{ width: 80 }}>Image</th>
-                <th style={{ width: 250 }}>Name</th>
-                <th>Category</th>
-                <th>Stock</th>
-                <th>Price</th>
-                <th>Ratings</th>
-                <th>Options</th>
-              </tr>
-            </thead>
-
-{
-    Products?.map(product => (
+    <Table
+      striped="columns"
+      responsive
+      borderless="false"
+      hover="true"
+      variant="lightgray"
+    >
+      <thead>
+        <tr>
+          <th style={{ width: 80 }}>Image</th>
+          <th style={{ width: 250 }}>Name</th>
+          <th>Category</th>
+          <th>Stock</th>
+          <th>Price</th>
+          <th>Ratings</th>
+          <th>Options</th>
+        </tr>
+      </thead>
+      {products?.map((product) => (
         <DashBoardTableCardProducts key={product._id} product={product} />
-    ))
-}
+      ))}
 
+    </Table>
+  );
+};
 
-          </Table>
-  )
-}
-
-export default DashBoardTableProducts
+export default DashBoardTableProducts;
--- client/src/pages/DashBoardUser/DashBoardUsersConteiner.jsx
+++ client/src/pages/DashBoardUser/DashBoardUsersConteiner.jsx
@@ -3,11 +3,11 @@ import DashBoardListUsers from "./ListUsers";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getClientAdminUsers } from '../../redux/actions';
-const DashBoardUsersConteiner = () => {      
+const DashBoardUsersConteiner = ({setActiveTab,activeTab,users,setActualPage}) => {      
     
     const dispatch = useDispatch()
     const clientAdmin = useSelector(state => state.clientAdmin)
-    const users = useSelector(state => state.clientAdminUsers)
+    // const users = useSelector(state => state.clientAdminUsers)
   
     useEffect(() => {
         dispatch(getClientAdminUsers(clientAdmin._id))
@@ -16,7 +16,7 @@ const DashBoardUsersConteiner = () => {
 return(
         <>
          <DashBoardNavUsers  />
-         <DashBoardListUsers />
+         <DashBoardListUsers setActiveTab={setActiveTab} activeTab={activeTab} users={users} setActualPage={setActualPage}/>
         </>
       
       ) 
