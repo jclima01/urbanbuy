dda16f17123ed5dca21c00cd42b9379444df9118
Juan Cruz Lima Caramico <50002478+jclima01@users.noreply.github.com>
2023-05-23 13:03:53 -0300

    Update DashBoard.jsx
    remove duplicate import link on dashboard

--- client/src/pages/DashBoard/DashBoard.jsx
+++ client/src/pages/DashBoard/DashBoard.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import DashBoardCardCategory from "../../Components/DashBoardCardCategory/DashBoardCardCategory";
 import DashBoardCardProducts from "../../Components/DashBoardCardProducts/DashBoardCardProducts";
 import DashBoardCardsUser from "../../Components/DashBoardCardsUser/DashBoardCardsUser";
