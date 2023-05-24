ec40b54a100c0324033e5630f5373b192d5aa596
ALBERTO EZEQUIEL IRACE <103865149+Eze323@users.noreply.github.com>
2023-05-24 17:43:31 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,13 +11,7 @@ app.use(express.json());
 app.use(cors())
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*"); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  });
+
 app.use(mainRouter)
 
 //Server Listen
