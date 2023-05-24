fbc90a3f9512349e5853acca63cec3b76273d9bd
ALBERTO EZEQUIEL IRACE <103865149+Eze323@users.noreply.github.com>
2023-05-24 04:53:04 -0300

    update deploy front

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: 'https://urbaybuy-front.up.railway.app/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    minify: true,
-    sourcemap: false,
-  },
+  // build: {
+  //   outDir: 'dist',
+  //   assetsDir: 'assets',
+  //   minify: true,
+  //   sourcemap: false,
+  // },
 });
