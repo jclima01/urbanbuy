0ecf8a49631298cd64e5fe04695db15686fb04f7
Juan Cruz Lima Caramico <juan.mdq22@gmail.com>
2023-05-24 18:14:19 -0300

    fix back deploy

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors())
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
-server.use((req, res, next) => {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', "*"); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
