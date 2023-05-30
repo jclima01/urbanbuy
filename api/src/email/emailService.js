d3eafbda1565864b09fdfafe1dd6d0550db1d017
Juan Cruz Lima Caramico <juan.mdq22@gmail.com>
2023-05-30 20:46:30 -0300

    fix

--- api/src/email/emailService.js
+++ api/src/email/emailService.js
@@ -1,14 +1,14 @@
-const nodemailer = require('nodemailer');
-const sendgridTransport = require('nodemailer-sendgrid-transport');
+// const nodemailer = require('nodemailer');
+// const sendgridTransport = require('nodemailer-sendgrid-transport');
 
-// Configura el transporte de SendGrid
-const transporter = nodemailer.createTransport(
-  sendgridTransport({
-    auth: {
-      api_key: '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',
-    },
-  })
-);
+// // Configura el transporte de SendGrid
+// const transporter = nodemailer.createTransport(
+//   sendgridTransport({
+//     auth: {
+//       api_key: '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',
+//     },
+//   })
+// );
 
-// Exporta el objeto 'transporter' para su uso en otras partes del proyecto
-module.exports = transporter;
+// // Exporta el objeto 'transporter' para su uso en otras partes del proyecto
+// module.exports = transporter;
