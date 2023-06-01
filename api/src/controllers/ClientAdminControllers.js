5376a1147e0fca47a92055d1af51795a277460da
Alejandro Villamayor <avillamayor720@gmail.com>
2023-06-01 20:40:04 -0300

    Privacidad a la apikey y formulacion de correo con estilos

--- api/src/controllers/ClientAdminControllers.js
+++ api/src/controllers/ClientAdminControllers.js
@@ -6,15 +6,54 @@ require("dotenv").config();
 
 const sendWelcomeEmail = (fullName, email) => {
   const mailOptions = {
-    from: 'urbanbuy8@gmail.com',
+    from: "urbanbuy8@gmail.com",
     to: email,
-    subject: '¡Bienvenido a nuestro sitio!',
-    text: `Hola ${fullName}, gracias por registrarte en nuestro sitio.`,
+    subject: "¡Bienvenido a UrbanBuy!",
+    html: '<html>\
+        <head>\
+          <style>\
+            /* Estilos CSS para el correo electrónico */\
+            body {\
+              font-family: Arial, sans-serif;\
+              background-color: #f4f4f4;\
+              color: #333;\
+            }\
+            \
+            h1 {\
+              color: #ff0000;\
+            }\
+            .highlight {\
+              font-weight: bold;\
+            }\
+          </style>\
+        </head>\
+        <body>\
+          <h1>Bienvenido a UrbanBuy. Hola ' + fullName + ',</h1>\
+          <p>Gracias por registrarte en nuestro sitio.</p>\
+          <p>UrbanBuy es un sitio web de comercio electrónico diseñado para facilitar la creación y gestión de tu propia tienda en línea.</p>\
+          <p>Con UrbanBuy, tienes acceso a una amplia gama de características y opciones que te permiten personalizar y administrar tu tienda de manera sencilla y eficiente.</p>\
+          <p>Aquí hay algunas características destacadas de UrbanBuy:</p>\
+          <ul>\
+            <li>Edición de estilos: Puedes personalizar el aspecto y la apariencia de tu página web utilizando opciones de edición de estilos. Cambia los colores, las fuentes, los diseños y más para reflejar la identidad de tu marca.</li>\
+            <li>Gestión de productos: UrbanBuy te permite cargar y administrar fácilmente tus productos. Puedes agregar descripciones, imágenes, categorías y precios para presentar tus productos de manera atractiva.</li>\
+            <li>Pasarelas de pago incorporadas: Facilitamos la incorporación de pasarelas de pago para que tus clientes puedan realizar compras de forma segura y conveniente. Aceptamos múltiples métodos de pago, como tarjetas de crédito y más.</li>\
+            <li>Carrito de compras: UrbanBuy proporciona un carrito de compras intuitivo y fácil de usar. Tus clientes pueden agregar productos, ver el resumen de su compra y finalizar el proceso de pago de manera rápida y sencilla.</li>\
+            <li>Administración de pedidos: Mantén un seguimiento de tus pedidos con la funcionalidad de administración de pedidos de UrbanBuy. Puedes ver y gestionar el estado de los pedidos, realizar seguimiento de envíos y comunicarte con los clientes.</li>\
+          </ul>\
+          <p>Estas son solo algunas de las muchas características que ofrece UrbanBuy. Te invitamos a explorar nuestro sitio y descubrir cómo podemos ayudarte a establecer y hacer crecer tu negocio en línea.</p>\
+          <p>No esperes más, ¡únete a UrbanBuy y comienza a vender tus productos hoy mismo!</p>\
+          <p>¡Gracias por elegir UrbanBuy!</p>\
+          <p><span class="highlight">El equipo de UrbanBuy</span></p>\
+        </body>\
+      </html>'
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.error("Error al enviar el correo electrónico de bienvenida:", error);
+      console.error(
+        "Error al enviar el correo electrónico de bienvenida:",
+        error
+      );
     } else {
       console.log("Correo electrónico de bienvenida enviado:", info.response);
     }
@@ -53,7 +92,7 @@ const ClientAdminLogin = async (email, password) => {
     const token = jwt.sign({ id: clientAdmin._id }, process.env.KEY_JWT, {
       expiresIn: "1h",
     });
-    console.log({ token, ...clientAdmin._doc})
+    console.log({ token, ...clientAdmin._doc });
     return await ClientAdmin.findOne({ email });
   } catch (error) {
     throw new Error(error.message);
@@ -63,7 +102,7 @@ const ClientAdminLogin = async (email, password) => {
 const ClientUpdate = async (clientId, fullName, email, password, logo) => {
   try {
     const client = await ClientAdmin.findById(clientId);
-    if(client) {
+    if (client) {
       client.fullName = fullName;
       client.email = email;
       client.password = password;
@@ -72,7 +111,7 @@ const ClientUpdate = async (clientId, fullName, email, password, logo) => {
     const updatedClient = await client.save();
     return updatedClient;
   } catch (error) {
-    throw new Error(error.message)
+    throw new Error(error.message);
   }
 };
 
@@ -85,11 +124,9 @@ const ClientDelete = async (clientId) => {
   }
 };
 
-
-
 module.exports = {
   ClientAdminRegister,
   ClientAdminLogin,
   ClientUpdate,
-  ClientDelete
+  ClientDelete,
 };
--- api/src/email/emailService.js
+++ api/src/email/emailService.js
@@ -1,14 +1,14 @@
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+require("dotenv").config();
 
 // Configura el transporte de SendGrid
 const transporter = nodemailer.createTransport(
   sendgridTransport({
     auth: {
-      api_key: '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',
+      api_key: process.env.API_KEY_EMAIL,
     },
   })
 );
 
-// Exporta el objeto 'transporter' para su uso en otras partes del proyecto
 module.exports = transporter;
