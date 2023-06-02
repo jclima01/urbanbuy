9ac728962bef7fd2117733174d5c7272317854a9
unknown <juan.mdq22@gmail.com]>
2023-06-02 14:27:56 -0300

    adding stripe structure

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "http://localhost:5173"); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', "*"); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,7 @@
 const { Router } = require("express");
-const express = require('express');
+
 // Importar todos los routers;
-const stripe = require("stripe")(
-    "sk_test_51NCdNdL2efsICo3fzbVNZmlNnJaJyRuDxAQrBTJBORiye8bCFNq6PqVwqNAcfnqXgmQ9dwySNJ2L6yQHqz17E2js0059R0fJ9h"
-  );
+
   
 const ProductRouter = require("./ProductsRoutes.js");
 const UsersRouter = require("./UserRoutes.js");
@@ -15,7 +13,6 @@ const OrderRouter = require("./OrderRoutes.js");
 const WebhookRouter = require("./WebhookRoutes.js");
 
 const mainRouter = Router();
-const endpointSecret = "whsec_080e4339ac22924a4100a26a1726c269cf0fdde488cc78c435c33e96e783a08a";
 
 // Configurar los routers
 
@@ -27,33 +24,6 @@ mainRouter.use("/category", CategoryRoutes);
 mainRouter.use("/reviews", ReviewRouter);
 mainRouter.use("/orders", OrderRouter);
 mainRouter.use("/webhook", WebhookRouter);
-
-// mainRouter.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-//   const sig = request.headers['stripe-signature'];
-
-//   let event;
-
-//   try {
-//     event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-//   } catch (err) {
-//     response.status(400).send(`Webhook Error: ${err.message}`);
-//     return;
-//   }
-
-//   // Handle the event
-//   switch (event.type) {
-//     case 'payment_intent.succeeded':
-//       const paymentIntentSucceeded = event.data.object;
-//       // Then define and call a function to handle the event payment_intent.succeeded
-//       break;
-//     // ... handle other event types
-//     default:
-//       console.log(`Unhandled event type ${event.type}`);
-//   }
-
-//   // Return a 200 response to acknowledge receipt of the event
-//   response.send();
-// });
 
 
 module.exports = mainRouter;
