0b0b0b4b4a02039f952ce6634e52523e841a53ff
unknown <juan.mdq22@gmail.com]>
2023-06-09 16:20:23 -0300

    working on cartwidget

---
+++ client/src/Components/EcommerceCliente/cartWidget/cartWidget.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { BsCart } from "react-icons/bs";
+import { useNavigate } from "react-router";
+import styles from "./CartWidget.module.css"
+import { useSelector } from "react-redux";
+const CartWidget = () => {
+
+  const navigate = useNavigate()
+
+
+  const calculateTotalQuantity = () => {
+    console.log(cart);
+    const totalQuantity = cart.reduce(
+      (count, product) => (count += product.quantity),
+      0
+    );
+    return totalQuantity;
+  };
+  return (
+    <div  className={styles.cartWidgetContainer} onClick={()=>navigate("/cart")}>
+      <p>
+        {calculateTotalQuantity() > 0 && <div>{calculateTotalQuantity()}</div>}
+      </p>
+        <BsCart/>
+    </div>
+  );
+};
+
+export default CartWidget;
