2c6d7a7c545a11210cf4c6b3e38c2a72864b2c30
emamanriquez <emamanriquez1991@gmail.com>
2023-06-13 19:54:13 -0300

    armando funcionalidad myOrders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ import SignIn from "./Components/SignIn/SignIn";
 import HomeEcommerce from "./Components/EcommerceCliente/HomeEcommerce";
 import ProductDetail from "./Components/ProductDetail/ProductDetail";
 import ShoppingCartContainer from "./Components/EcommerceCliente/ShoppingCart/ShoppingCartContainer";
-import LoginClient from './Components/EcommerceCliente/LoginClient/LoginClient'
+import LoginClient from "./Components/EcommerceCliente/LoginClient/LoginClient";
 import Payment from "./Components/EcommerceCliente/ShoppingCart/Payment/Payment";
 import SignInClient from "./Components/EcommerceCliente/SignInClient/SignInClient";
 import EcommerceUser from "./Components/EcommerceCliente/EcommerceUser";
@@ -22,11 +22,12 @@ import PaymentSuccess from "./Components/EcommerceCliente/ShoppingCart/Payment/P
 import PaymentCanceled from "./Components/EcommerceCliente/ShoppingCart/Payment/PaymentCanceled";
 import { useSelector } from "react-redux";
 import UserArea from "./Components/EcommerceCliente/UserArea/UserArea";
+import MyOrders from "./Components/EcommerceCliente/MyOrders/MyOrders";
 
 function App() {
   const location = useLocation();
-  const user = useSelector(state => state.user)
-  const clientAdmin = useSelector(state => state.clientAdmin)
+  const user = useSelector((state) => state.user);
+  const clientAdmin = useSelector((state) => state.clientAdmin);
   return (
     <>
       <div className="d-flex w-100">
@@ -46,23 +47,31 @@ function App() {
           {location.pathname === "/dashboard/Settings" && <NavBarDashBoard />}
 
           <Routes>
-            <Route path="/paymentSuccess" element={<PaymentSuccess clientAdmin={clientAdmin} user={user} />} />
-            <Route path="/paymentCanceled" element={<PaymentCanceled clientAdmin={clientAdmin} user={user}/>} />
+            <Route
+              path="/paymentSuccess"
+              element={<PaymentSuccess clientAdmin={clientAdmin} user={user} />}
+            />
+            <Route
+              path="/paymentCanceled"
+              element={
+                <PaymentCanceled clientAdmin={clientAdmin} user={user} />
+              }
+            />
             <Route path="/payment" element={<Payment />} />
             <Route path="/" element={<Home />} /> {/* LadingPage */}
             <Route path="/login" element={<FormLogin />} />
-            <Route path="/loginClient" element={<LoginClient/>}/>
+            <Route path="/loginClient" element={<LoginClient />} />
             <Route path="/singin" element={<SignIn />} />
-
-            <Route path="/signInClient" element={<SignInClient/>}/>
-            <Route path="/:domain/s" element={<EcommerceUser/>}/>
+            <Route path="/signInClient" element={<SignInClient />} />
+            <Route path="/:domain/s" element={<EcommerceUser />} />
             <Route path="/dashBoard" element={<DashBoard />} />
             <Route path="/dashBoard/User" element={<DashBoardUser />} />
             <Route path="/dashBoard/Edit" element={<DashBoardEdit />} />
             <Route path="/dashBoard/Products" element={<DashBoardProducts />} />
             <Route path="/dashBoard/Shipping" element={<DashBoardShipping />} />
             <Route path="/dashBoard/Settings" element={<DashBoardSettings />} />
-<Route path="/userArea" element={<UserArea/>}/>
+            <Route path="/userArea" element={<UserArea />} />
+            <Route path="/myOrders" element={<MyOrders />} />
             <Route path="/homecliente" element={<HomeEcommerce />} />
             <Route path="/:domain" element={<HomeEcommerce />} />
             <Route path="/product/:productId" element={<ProductDetail />} />
