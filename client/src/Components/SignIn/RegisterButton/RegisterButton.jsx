c2647b61d0e355ebff6640cff20cb3e388c282ae
Juan Cruz Lima Caramico <50002478+jclima01@users.noreply.github.com>
2023-06-14 17:15:50 -0300

    Update RegisterButton.jsx

--- client/src/Components/SignIn/RegisterButton/RegisterButton.jsx
+++ client/src/Components/SignIn/RegisterButton/RegisterButton.jsx
@@ -8,7 +8,7 @@ const RegisterButton = () => {
     loginWithRedirect({
         connection: "google-oauth2",
         screen_hint: "signup",
-        redirectUri: "https://urbanbuy.netlify.app/login",
+        redirectUri: "https://urbanbuy.netlify.app/singin",
       });
    
   };
