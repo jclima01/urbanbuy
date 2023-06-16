6a36d565ea3531067e09db748cff1dbbf3953f63
Juan Cruz Lima Caramico <juan.mdq22@gmail.com>
2023-06-16 18:04:12 -0300

    fix review navigation

--- client/src/Components/ProductReview/ProductReview.jsx
+++ client/src/Components/ProductReview/ProductReview.jsx
@@ -1,21 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { setReview } from '../../redux/actions';
-import style from "./ProductReview.module.css"
-
+import { setReview } from "../../redux/actions";
+import style from "./ProductReview.module.css";
 
 const ProductReview = () => {
   //Agregar validaciones!!!
-const dispatch = useDispatch();
-const navigate = useNavigate();
-  
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const clientAdmin = useSelector((state) => state.clientAdmin);
   const [rating, setRating] = useState(0);
-  const [comment, setComment] = useState('');
-   
-  const productId = useSelector((state) => state.product)._id;
-  const userId  = useSelector((state) => state.user)._id; 
+  const [comment, setComment] = useState("");
 
+  const productId = useSelector((state) => state.product)._id;
+  const userId = useSelector((state) => state.user)._id;
 
   const handleRatingChange = (event) => {
     setRating(event.target.value);
@@ -27,30 +25,38 @@ const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (rating === 0 || comment === '' ) {
-      alert("Por favor, selecciona una calificación y deja un comentario antes de enviar la reseña.");
+    if (rating === 0 || comment === "") {
+      alert(
+        "Por favor, selecciona una calificación y deja un comentario antes de enviar la reseña."
+      );
       return;
     }
     dispatch(setReview(productId, userId, comment, rating)).finally(() => {
-      navigate("/homecliente");
+      navigate(`/${clientAdmin.domain}`);
     });
-    console.log('Rating:', rating);
-    console.log('Comment:', comment);
-    console.log('UserId:', userId);
-    console.log('ProductId:', productId);
+    console.log("Rating:", rating);
+    console.log("Comment:", comment);
+    console.log("UserId:", userId);
+    console.log("ProductId:", productId);
     // Reiniciamos los campos después de enviar la reseña
     setRating(0);
-    setComment('');
+    setComment("");
   };
 
   return (
-    <div className={style.formReview} >
+    <div className={style.formReview}>
       <h2>Deja una reseña del producto</h2>
       <form onSubmit={handleSubmit}>
         <div className={style.divCal}>
           <label>Calificación:</label>
-          <select className={style.selectReview} value={rating} onChange={handleRatingChange}>
-            <option className={style.optionOne} value={0}>Selecciona una calificación</option>
+          <select
+            className={style.selectReview}
+            value={rating}
+            onChange={handleRatingChange}
+          >
+            <option className={style.optionOne} value={0}>
+              Selecciona una calificación
+            </option>
             <option value={1}>★ </option>
             <option value={2}>★ ★</option>
             <option value={3}>★ ★ ★</option>
@@ -60,9 +66,16 @@ const navigate = useNavigate();
         </div>
         <div className={style.divCal}>
           <label>Comentario:</label>
-          <textarea className={style.textareaReview} value={comment} onChange={handleCommentChange} rows={4} />
+          <textarea
+            className={style.textareaReview}
+            value={comment}
+            onChange={handleCommentChange}
+            rows={4}
+          />
         </div>
-        <button className={style.btnReview} type="submit">Enviar reseña</button>
+        <button className={style.btnReview} type="submit">
+          Enviar reseña
+        </button>
       </form>
     </div>
   );
